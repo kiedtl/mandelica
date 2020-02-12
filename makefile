4c5d926e36dfb81c4d6c10506335147d9f6768fa
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2020-02-11 20:36:14 -0500

    mandel.c: add support for ff

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ NAME        = mandel
 INC         = -I. -Isub/ccommon/
 
 CC          = gcc
-CFLAGS      = -O4 $(INC)
+CFLAGS      = -O0 $(INC) -ggdb
 
 $(NAME): $(NAME).o
 	$(CC) $(CFLAGS) -o $@ $^
--- mandel.c
+++ mandel.c
@@ -1,11 +1,32 @@
+#include <arpa/inet.h>
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "types.h"
 
 int
 main(int argc, char **argv)
 {
+	u32 height = 31;
+	u32 width  = 80;
+
 	usize ctr = 0;
 	float i, j, r;
+
+	/* initialize farbfeld stuff */
+	u32 buf;
+	fputs("farbfeld", stdout);                   /* magic value */
+	buf = htonl(width);
+	fwrite(&buf, sizeof(buf), 1, stdout);        /* width */
+	buf = htonl(height);
+	fwrite(&buf, sizeof(buf), 1, stdout);        /* height */
+
+	u16 *data = realloc(NULL, width * ((sizeof("RGBA") - 1) * sizeof(u16)));
+	if (data == NULL) {
+		fprintf(stderr, "error: realloc: ");
+		perror(NULL);
+		return 1;
+	}
 
 	for (float y = -16; y < 15; ++y) {
 		for (float x = 0; x < 84; ++x) {
@@ -20,9 +41,15 @@ main(int argc, char **argv)
 					break;
 			}
 
-			putchar(62 - ctr);
+			//putchar(62 - ctr);
+			u16 color = 0xffff;
+			if (ctr == 30) color = 0x0000;
+			data[4 * (size_t) x + 0] = htons(color);
+			data[4 * (size_t) x + 1] = htons(color);
+			data[4 * (size_t) x + 2] = htons(color);
+			data[4 * (size_t) x + 3] = htons(0xffff);
 		}
 
-		putchar(10); /* newline */
+		fwrite(data, sizeof(u16), width * 4, stdout);
 	}
 }
